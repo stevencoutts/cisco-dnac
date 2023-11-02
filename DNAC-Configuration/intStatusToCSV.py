1f108b124157496c0bc99d14c471a3521d1606eb
Steven Coutts <stevec@couttsnet.com>
2023-11-02 16:35:30 +0000

    Tidy python

--- DNAC-Configuration/intStatusToCSV.py
+++ DNAC-Configuration/intStatusToCSV.py
@@ -15,20 +15,20 @@ with open(inputFilename) as f:
         # Look for line before interface output begins, it should start with the word Port
         # If it is that line, set begin variable to True
         # We should only detect this once, the other times we can ignore
-        if (line.strip().startswith("Port") and begin == False):
+        if line.strip().startswith("Port") and begin is False:
             print("-- Beginning of interface output detected")
             print("--")
             begin = True
-        # If we have already found the beginning of the output, it isn't a --More-- line, and it isn't the headers repeated again
-        # Also ignore port-channels
-        if (begin == True and not (("--More--") in line.strip()) and not line.strip().startswith("Po")):
+        # If we have already found the beginning of the output, it isn't a --More-- line
+        # and it isn't the headers repeated again, also ignore port-channels
+        if begin is True and "--More--" not in line.strip() and not line.strip().startswith("Po"):
             # Output from show int status is based on character count
             interface = line.strip()[0:9]
             description = line.strip()[10:28]
             status = line.strip()[29:41]
             vlan = line.strip()[42:53]
             # Debug Output
-            if (debug == True):
+            if debug is True:
                 print(interface.strip() + "," + description.strip() + "," + status.strip() + "," + vlan.strip())
             with open(mappingFilename) as m:
                 # Loop
@@ -39,8 +39,8 @@ with open(inputFilename) as f:
                     if not mline:
                         break
                     # if the mapping line is for this vlan
-                    if (vlan.strip() in mline.split(",")[0]):
+                    if vlan.strip() in mline.split(",")[0]:
                         # Debug
-                        if (debug == True):
-                            print (mline.strip())
-                        print ("--")
+                        if debug is True:
+                            print(mline.strip())
+                        print("--")
